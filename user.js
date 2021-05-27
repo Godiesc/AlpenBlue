42a70a147ca2086b7c458e7bba5d1c84ad6b00ba
GodieGun <diego_sc25@outlook.com>
2021-05-27 09:41:58 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,3 +1,2 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
 user_pref("svg.context-properties.content.enabled", true); 
-user_pref("browser.display.background_color", '#16143c');
